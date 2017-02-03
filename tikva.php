2f23ecf187044e2b05281bf316868d674fcd1460
Ralf Geschke <ralf@kuerbis.org>
2017-02-03 01:26:33 +0100

    Testing events and add configuration of footer

--- tikva.php
+++ tikva.php
@@ -6,4 +6,40 @@ use Grav\Common\Theme;
 class Tikva extends Theme
 {
     // Access plugin events in this class
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onThemeInitialized' => ['onThemeInitialized', 0],
+        ];
+    }
+
+   
+
+    public function onThemeInitialized()
+    {
+        if ($this->isAdmin()) {
+            //$this->active = false;
+            $this->grav['debugger']->addMessage("Hello from Tikva in Admin only!");
+
+            return;
+        }
+
+        $this->grav['debugger']->addMessage("Hello from Tikva!");
+
+        //$this->enable([
+        //    'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+        //]);
+    }
+
+    public function onTwigSiteVariables()
+    {
+        $this->grav['assets']
+            ->addCss('plugin://css/mytheme-core.css')
+            ->addCss('plugin://css/mytheme-custom.css');
+
+        $this->grav['assets']
+            ->add('jquery', 101)
+            ->addJs('theme://js/jquery.myscript.min.js');
+    }
 }
